2869234276087979fb667bdc3136411d7882ff03
dassshic <goryaeva.daria@gmail.com>
2022-12-28 15:02:19 +0300

    homework (2) done

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
-        Car ladaGranta = new Car ();
-        ladaGranta.brand = "Lada";
-        ladaGranta.model = "Granta";
-        ladaGranta.country = "Russia";
-        ladaGranta.color = "yellow";
-        ladaGranta.engineVolume = 1.7;
-        ladaGranta.year = 2015;
+        Car ladaGranta = new Car ("Lada", "Granta", 1.7, "yellow", 2015,"Russia");
 
-        Car audiA850LTDIQuattro = new Car();
-        audiA850LTDIQuattro.brand = "Audi";
-        audiA850LTDIQuattro.model = "A8 50 L TDI quattro";
-        audiA850LTDIQuattro.year = 2020;
-        audiA850LTDIQuattro.country = "Germany";
-        audiA850LTDIQuattro.color = "black";
-        audiA850LTDIQuattro.engineVolume = 3.0;
+        Car audiA850LTDIQuattro = new Car("Audi","A8 50 L TDI quattro",3.0,"black",2020,"Germany");
 
-        Car bmwZ8 = new Car();
-        bmwZ8.brand = "BMW";
-        bmwZ8.model = "Z8";
-        bmwZ8.country = "Germany";
-        bmwZ8.year = 2021;
-        bmwZ8.color = "black";
-        bmwZ8.engineVolume = 3.0;
+        Car bmwZ8 = new Car("BMW","Z8",3.0,"black",2021,"Germany");
 
-        Car kiaSportage4 = new Car();
-        kiaSportage4.brand = "KIA";
-        kiaSportage4.model = "Sportage 4 generatons";
-        kiaSportage4.year = 2018;
-        kiaSportage4.country = "South Korea";
-        kiaSportage4.color = "red";
-        kiaSportage4.engineVolume = 2.4;
+        Car kiaSportage4 = new Car("KIA","Sportage 4 generatons",2.4,"red",2018,"South Korea");
 
-        Car hyundaiAvante = new Car();
-        hyundaiAvante.brand = "Hyundai";
-        hyundaiAvante.model = "Avante";
-        hyundaiAvante.country = "South Korea";
-        hyundaiAvante.color = "orange";
-        hyundaiAvante.engineVolume = 1.6;
-        hyundaiAvante.year = 2016;
+        Car hyundaiAvante = new Car("Hyundai","Avante",1.6,"orange",2016,"South Korea");
 
         System.out.println(ladaGranta);
         System.out.println(audiA850LTDIQuattro);
